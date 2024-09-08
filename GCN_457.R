4e83592dc159379ab06490639d53100900aba15d
Anish Kelam <60801871+anishkelam@users.noreply.github.com>
2024-09-07 23:02:09 -0400

    Add files via upload

---
+++ GCN_457.R
@@ -0,0 +1,25 @@
+library(BioNERO)
+
+dataset <- read.csv("GLDS-457_rna_seq_DEG_Normalized_Filtered_Counts.csv")
+# Set the first column as the row names
+rownames(dataset) <- dataset[,1]
+
+# Remove the first column as it's now being used as row names
+dataset <- dataset[,-1]
+
+dataset <- dataset[ , -c(7:12)]
+
+sft <- SFT_fit(dataset, net_type = "signed hybrid", cor_method = "pearson")
+
+net <- exp2gcn(
+  dataset, net_type = "signed hybrid", SFTpower = 11, 
+  cor_method = "pearson")
+
+hubs <- get_hubs_gcn(dataset, net)
+print(hubs)
+
+# Extract the first column (Genes) from the 'hubs' data frame and convert to character
+genes_list <- hubs$Gene
+
+# Export the extracted column to a plain text file, each item on a new line
+writeLines(genes_list, "457_Hub_Genes.txt")
